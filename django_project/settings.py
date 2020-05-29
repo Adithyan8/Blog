1e88e08970ffb3709c7b5cf40b03e7d30b5910ce
AcumensaDev <adi@acumensa.co>
2020-05-29 16:54:31 +0530

    Updated static root

--- django_project/settings.py
+++ django_project/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')   #where images are saved. profile_pics saved inside the media directory
 
