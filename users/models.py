c3191d23114cd849ad981597758afee22b0f0739
AcumensaDev <adi@acumensa.co>
2020-06-03 14:25:16 +0530

    fixing whitenoise - 2

--- users/models.py
+++ users/models.py
@@ -9,11 +9,3 @@ class Profile(models.Model):
     def __str__(self):
         return f"{self.user.username} Profile"
     
-    def save(self,*args,**kwargs):                     #This function to create custom save function
-        super().save(*args,**kwargs)
-        img=Image.open(self.image.path)
-                                                 #used to resize uploaded image to reduce size of the image file
-        if img.height>200 or img.width>200 : 
-            output_size=(200,200)
-            img.thumbnail(output_size)             #resizes the image
-            img.save(self.image.path)
